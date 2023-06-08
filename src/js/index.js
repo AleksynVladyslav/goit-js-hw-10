f577a11e3b4085a62786559f3ad9c2fa95097b5c
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-06-08 08:07:58 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,70 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-export { selectRef };
+export { selectRef, API_KEY, createOptions };
+
+const API_KEY = `live_xgJbTp7HDySw1CIfSTFuu3jEoMUIvb2dpZqt0pkmAkb3BCQRTNCyGVNsaqN2MP7l`;
+const URL = 'https://api.thecatapi.com/v1';
+
 const selectRef = document.querySelector('.breed-select');
 const articleConteinerRef = document.querySelector('.cat-info');
+const loadRef = document.querySelector('.loader');
+const errorRef = document.querySelector('.error');
 
-fetch(
-  'https://api.thecatapi.com/v1/breeds?api_key=live_xgJbTp7HDySw1CIfSTFuu3jEoMUIvb2dpZqt0pkmAkb3BCQRTNCyGVNsaqN2MP7l'
-)
-  .then(response => response.json())
-  .then(cats => {
-    fetchBreeds(cats);
-  })
-  .catch(err => console.log(console.log(err)));
+errorRef.classList.add('is-hidden');
+selectRef.classList.add('is-hidden');
 
 selectRef.addEventListener('change', onSelectCat);
+fetchBreeds();
 
-function onSelectCat(event) {
-  const selectedCat = event.target.value;
-  console.log(selectedCat);
-  fetchCatByBreed(selectedCat);
-  fetch(
-    `https://api.thecatapi.com/v1/images/search?api_key=live_xgJbTp7HDySw1CIfSTFuu3jEoMUIvb2dpZqt0pkmAkb3BCQRTNCyGVNsaqN2MP7lbreed_ids=${selectedCat}&limit=1`
-  )
+function fetchBreeds() {
+  fetch(`${URL}/breeds?api_key=${API_KEY}`)
     .then(response => response.json())
-    .then(cats => fetchCatByBreed(cats))
-    .then(cat => console.log(createArticleMurcup(cat)))
-    .catch(err => console.log(err));
+    .then(catsInfo => {
+      selectRef.classList.remove('is-hidden');
+      loadRef.classList.add('is-hidden');
+      createOptions(catsInfo);
+    })
+    .catch(() => {
+      selectRef.classList.add('is-hidden');
+      errorRef.classList.remove('is-hidden');
+      loadRef.classList.add('is-hidden');
+    });
 }
 
-function createArticleMurcup(data) {
-  articleConteinerRef.innerHTML = `<img src = ${data.url}><h1>${data.name}</h1><p>${data.description}</p><p>${data.temperament}</P>`;
+function createOptions(catsInfo) {
+  const murkup = catsInfo
+    .map(catInfo => {
+      return `<option value = ${catInfo.id}>${catInfo.name} </option>`;
+    })
+    .join('');
+  selectRef.innerHTML = murkup;
+}
+
+function onSelectCat(event) {
+  articleConteinerRef.innerHTML = ' ';
+  const selectedCat = event.target.value;
+  // console.log(selectedCat);
+  fetch(
+    `${URL}/images/search?api_key=${API_KEY}&breed_id=${selectedCat}&limit=1`
+  )
+    .then(response => response.json())
+    .then(cat => {
+      const name = cat[0].breeds[0].name;
+      const url = cat[0].url;
+      const description = cat[0].breeds[0].description;
+      const temperament = cat[0].breeds[0].temperament;
+      return { name, url, description, temperament };
+    })
+    .then(data => {
+      articleConteinerRef.innerHTML = '';
+      createArticleMurcup(data);
+    })
+    .catch(() => errorRef.classList.remove('is-hidden'));
+}
+
+function createArticleMurcup({ name, url, description, temperament }) {
+  loadRef.classList.remove('is-hidden');
+  setTimeout(() => {
+    loadRef.classList.add('is-hidden');
+
+    articleConteinerRef.innerHTML = `<img src = ${url} alt = ${name} width='400' ><div class= "conteiner"><h1 class ="name"> ${name}</h1><p class ="description">  ${description}</p><p class ="temperament"> <span>Temperament:</span> ${temperament}</P></div>`;
+  }, 1000);
 }
